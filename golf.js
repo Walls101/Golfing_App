0ecbc0a069d87ce72d44818a9cd7de3aafddc280
Alex Paredes <alexparedes83@gmail.com>
2022-10-07 07:51:19 -0600

    the select drop box has the course names

--- golf.js
+++ golf.js
@@ -1,11 +1,19 @@
+let courses;
 function getAvailableCourses() {
-    return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
-      function (response) {
-        return response.json();
-      }
-    );
-}
+    return fetch('https://golf-courses-api.herokuapp.com/courses')
+    .then((response) => response.json()).then(data => data).then(data => callMe(data))
+} 
 
+function callMe(data) {
+    courses = data.courses;
+    let courseOptionsHtml = '';
+    courses.forEach((course) => {
+        courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+    });
+    
+    document.getElementById('course-select').innerHTML = courseOptionsHtml;
+}
+getAvailableCourses()
 
 
 // Not sure why this is here :/
