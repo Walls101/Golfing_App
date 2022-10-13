c5744b9733be88db98ef37d08cd26713d2e82c93
Alex Paredes <alexparedes83@gmail.com>
2022-10-12 21:15:37 -0600

    golf.js step 4 into async and await for courses

--- golf.js
+++ golf.js
@@ -1,11 +1,19 @@
-let courses;
-function getAvailableCourses() {
+async function getAvailableCourses() {
     return fetch('https://golf-courses-api.herokuapp.com/courses')
-    .then((response) => response.json()).then(data => callMe(data))
+    .then((response) => response.json()).then(data => data.courses)
 } 
 
-function callMe(data) {
-    courses = data.courses;
+async function rendorAvailableCourses() {
+    const courses = await getAvailableCourses();
+    courseName(courses);
+}
+
+async function initialLoad() {
+    await rendorAvailableCourses();
+}
+
+
+function courseName(courses) {
     let courseOptionsHtml = '';
     courses.forEach((course) => {
         courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
@@ -13,5 +21,26 @@ function callMe(data) {
     
     document.getElementById('course-select').innerHTML = courseOptionsHtml;
 }
+
 getAvailableCourses()
 
+function getCourseData(){
+    return fetch('https://golf-courses-api.herokuapp.com/courses/'+ info)
+    .then((response) => response.json()).then(data => courseData(data))
+}
+
+function courseData(data) {
+    let teeBoxes = data;
+    let teeBoxSelectHtml = '';
+    teeBoxes.forEach(function (teeBox, index) {
+    teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
+     teeBox.totalYards
+    } yards</option>`
+    });
+
+    document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
+}
+
+initialLoad();
+
+//confused with step 4 and making the teebox show in the select box.
