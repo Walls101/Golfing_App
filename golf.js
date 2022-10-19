544be0d80eda17c1308a27660574a9c8710640c7
Alex Paredes <alexparedes83@gmail.com>
2022-10-19 09:45:11 -0600

    commit in golf Teebox options

--- golf.js
+++ golf.js
@@ -1,18 +1,22 @@
-async function getAvailableCourses() {
-    // return fetch('https://golf-courses-api.herokuapp.com/courses')
-    // .then((response) => response.json()).then(data => data.courses)
-    const response = await fetch('https://golf-courses-api.herokuapp.com/courses');
-    const data = await response.json();
-    return data.courses;
+let id = 0;
+
+function getAvailableCourses(cb) {
+    fetch('https://golf-courses-api.herokuapp.com/courses')
+    .then((response) => response.json())
+    .then(data => cb(data.courses))
+    // const response = await fetch('https://golf-courses-api.herokuapp.com/courses');
+    // const data = await response.json();
+    // cb(data.courses);
 } 
 
-async function rendorAvailableCourses() {
-    const courses = await getAvailableCourses();
+function rendorAvailableCourses(courses) {
+    // const courses = await getAvailableCourses();
     courseName(courses);
     // getCourseData(courses);
 }
 
 function courseName(courses) {
+    console.log("courseName")
     let courseOptionsHtml = '';
     courses.forEach((course) => {
         courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`
@@ -23,39 +27,55 @@ function courseName(courses) {
     var select = document.getElementById('course-select');
     var value = select.options[select.selectedIndex].value;
     console.log(value);
-    getCourseData(value);
-    // let courseID = course.id;
-    // getCourseData(courseID);
+    getCourseData(value, rendorCourse);
+    // rendorCourse(getCourseData(value));
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-async function getCourseData(course){
-    const response = await fetch(`https://golf-courses-api.herokuapp.com/courses/${course}`);
-    const data = await response.json();
-    console.log(data);
-    return courseData(data);
+function getCourseData(course, cb){
+    console.log("getCourseData")
+    fetch(`https://golf-courses-api.herokuapp.com/courses/${course}`)
+    .then((response) => response.json())
+    .then(data => cb(data.data.holes))
+    // const data = await response.json();
+    // console.log(data);
+    // return data.holes;
 }
 
+function rendorCourse(courseName){
+    console.log(courseName)
+    console.log("renderCourse")
+    const teeBoxes = courseName;
+    console.log(teeBoxes);
+    //teeBoxes is undefined ***
+    courseData(teeBoxes);
+}
 
 function courseData(data) {
+    console.log("CourseData")
 //Problem is here. Still trying to enter the data here into the dropbox.**
-    
-    let teeBoxes = data;
+    // having issues with map aa cannot read properties of undefied(reading'holes')
+    let info = data.map(holeItem => holeItem.teeBoxes[0]);
     let teeBoxSelectHtml = '';
-    teeBoxes.forEach(function (teeBox, index) {
-    teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${teeBox.totalYards} yards</option>`
+    info.forEach((teeBox, index) => {
+        teeBoxSelectHtml += `<option value="${index}">PRO, ${teeBox.yards} yards</option>`
+        teeBoxSelectHtml += `<option value="${index}">CHAMPION, ${teeBox.yards} yards</option>`
+        teeBoxSelectHtml += `<option value="${index}">MEN, ${teeBox.yards} yards</option>`
+        teeBoxSelectHtml += `<option value="${index}">WOMEN, ${teeBox.yards} yards</option>`
+
     });
 
     document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
 }
 
 
-async function initialLoad() {
-    await rendorAvailableCourses();
+function initialLoad() {
+    // await rendorAvailableCourses();
+    getAvailableCourses(rendorAvailableCourses)
+    // await rendorCourse();
 }
 
 initialLoad();
-
 //confused with step 4 and making the teebox show in the select box.
 //But I got the data course data
